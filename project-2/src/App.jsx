2aaa5c770212aee165d702401f59eef8a4e84d62
Kenta Yamamoto <kenta12003@gmail.com>
2023-11-15 01:56:36 -0800

    Translated most of homepage into React and added image component
    (apologies for the big commit)
    -Translated most of the HTML/CSS into React code for the homepage
    -Added a new component that loads an image based on an index/file/class

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,33 +1,332 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ImageComp from './ImageComp'
+
+
+import homeImg from './assets/images/home-img.png';
+import homeImgB from './assets/images/home-img1.png';
+
+import './App.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  // console.log(imagePaths[0]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* header section starts  */}
+
+      <header>
+
+          <section className="flex">
+
+              <a href="#" className="logo"><i className="fas fa-utensils"></i>food</a>
+
+              <div id="menu-bar" className="fas fa-bars"></div>
+
+              <nav className="navbar">
+                  <a href="#home">home</a>
+                  <a href="#speciality">speciality</a>
+                  <a href="#popular">popular</a>
+                  <a href="#gallery">gallery</a>
+                  <a href="#review">review</a>
+              
+              </nav>
+
+          </section>
+
+      </header>
+
+      {/* header section ends */}
+
+      {/* home section starts  */}
+
+
+      <div className="home-container">
+
+          <section className="home" id="home">
+
+              <div className="content">
+                  <h3>UC Berkeley Food Ranking</h3>
+                  <p>Explore top-rated eateries around UC Berkeley, from cozy cafes to gourmet delights, rated by students for authentic flavors and unique dining experiences.</p>
+                  <a href="#" className="btn">discover now</a>
+              </div>
+          
+              <div className="image">
+                  <img src={homeImg} alt=""/>
+              </div>
+          
+          </section>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+ 
+      {/* home section ends */}
+
+      {/* speciality section starts  */}
+
+      <section className="speciality" id="speciality">
+
+          <h1 className="heading"> our <span>Ranking</span> </h1>
+
+          <div className="box-container">
+
+              <div className="box">
+                  <ImageComp imageClass={"image"} folder={"s-imgs-b"} index={0}></ImageComp>
+                  <div className="content">
+                      <img src={homeImgB} alt=""/>
+                      <h3>burger</h3>
+                      <p>Dive into the heart of American cuisine with our selection of UC Berkeley's finest burgers. Each patty is a blend of quality and flavor, grilled to perfection and served with fresh, locally-sourced toppings.</p>
+                  </div>
+              </div>
+              <div className="box">
+                  <img className="image" src="images/s-img-2.jpg" alt=""/>
+                  <div className="content">
+                      <img src={"../assets/images/s-imgs-a/s-2.png"} alt=""/>
+                      <h3>pizza</h3>
+                      <p>Slice into Berkeley's best pizzas, where traditional methods meet innovative toppings. From classNameic Margheritas to adventurous flavor combinations, discover the pizzerias that students can't stop talking about.</p>
+                  </div>
+              </div>
+              <div className="box">
+                  <img className="image" src="images/s-img-3.jpg" alt=""/>
+                  <div className="content">
+                      <img src={"../assets/images/s-imgs-a/s-3.png"} alt=""/>
+                      <h3>ice-cream</h3>
+                      <p>Cool down with a scoop from Berkeley's best ice cream parlors. Artisanal flavors, classNameic cones, and everything in between—treat yourself to the creamiest delights near campus.</p>
+                  </div>
+              </div>
+              <div className="box">
+                  <img className="image" src="images/s-img-4.jpg" alt=""/>
+                  <div className="content"/>
+                      <img src={"../assets/images/s-imgs-a/s-4.png"} alt=""/>
+                      <h3>drinks</h3>
+                      <p>Refresh your palate with a variety of drinks ranked by Berkeley students. From energizing coffees to smoothie bliss, quench your thirst with the best sips in town.</p>
+                  </div>
+              </div>
+              <div className="box">
+                  <img className="image" src="images/s-img-5.jpg" alt=""/>
+                  <div className="content">
+                      <img src={"../assets/images/s-imgs-a/s-5.png"} alt=""/>
+                      <h3>sweets</h3>
+                      <p>Indulge in a slice of heaven with Berkeley's top-rated cakes. Whether you're after a rich chocolate layer cake or a light and fruity sponge, our guide has the sweetest spots around campus.</p>
+                  </div>
+              </div>
+              <div className="box">
+                  <img className="image" src="images/s-img-6.jpg" alt=""/>
+                  <div className="content">
+                      <img src={"../assets/images/s-imgs-a/s-6.png"} alt=""/>
+                      <h3>breakfast</h3>
+                      <p>Start your day the Berkeley way with breakfast spots that fuel the mind and satisfy the soul. From hearty omelets to organic acai bowls, find the perfect place to kickstart your morning.</p>
+                  </div>
+              </div>
+      </section>
+          {/* </div>
+
+      </section>
+*/}
+      {/* speciality section ends */}
+
+      {/* popular section starts  */}
+
+{/*
+      <section className="popular" id="popular">
+
+          <h1 className="heading"> UC Berkeley <span>6 Best </span>Restaurants</h1>
+
+          <div className="box-container">
+
+              <div className="box">
+                  
+                  <img src="images/p-1.jpg" alt=""/>
+                  <h3>Super Duper Burgers</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                  </div>
+                  <a href="#" className="btn">check now</a>
+              </div>
+              <div className="box">
+                  
+                  <img src="images/p-2.jpg" alt=""/>
+                  <h3>U :Dessert Story</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                  
+                  </div>
+                  <a href="#" className="btn">check now</a>
+              </div>
+              <div className="box">
+              
+                  <img src="images/p-3.jpg" alt=""/>
+                  <h3>YiFang Taiwan Fruit Tea</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                  
+                  </div>
+                  <a href="#" className="btn">check now</a>
+              </div>
+              <div className="box">
+                  
+                  <img src="images/p-4.jpg" alt=""/>
+                  <h3>Noodle Dynasty</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                    
+                  </div>
+                  <a href="#" className="btn">check now</a>
+              </div>
+              <div className="box">
+                  
+                  <img src="images/p-5.jpg" alt=""/>
+                  <h3>Sliver Pizzeria</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                  </div>
+                  <a href="#" className="btn">check now</a>
+              </div>
+              <div className="box">
+                  
+                  <img src="images/p-6.jpg" alt=""/>
+                  <h3>top dog</h3> 
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                  </div>
+                  <a href="#" className="btn">check now</a>
+              </div>
+
+          </div>
+
+      </section>
+*/}
+      {/* popular section ends */}
+
+
+
+      {/* review section starts  */}
+{/*
+      <section className="review" id="review">
+
+          <h1 className="heading">  food  <span> judges</span> </h1>
+
+          <div className="box-container">
+
+              <div className="box">
+                  <img src="images/pic1.png" alt="">
+                  <h3>Kenta</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="far fa-star"></i>
+                  </div>
+                  <p>Kenta brings a world-traveled palate to Berkeley's food scene. With a keen sense for spices and a love for sustainable dining, he searches for dishes that tell a story and leave a lasting impression.</p>
+              </div>
+              <div className="box">
+                  <img src="images/pic2.png" alt="">
+                  <h3>Luka</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                  </div>
+                  <p>Luka's culinary expertise is grounded in technique and tradition. A stickler for authenticity, he appreciates eateries that stay true to the roots of their dishes while adding a touch of local flair.</p>
+              </div>
+              <div className="box">
+                  <img src="images/pic3.png" alt="">
+                  <h3>Brain</h3>
+                  <div className="stars">
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="far fa-star"></i>
+                  </div>
+                  <p>Brian's approach to food is all about innovation and experience. He champions restaurants that push the envelope with bold flavors and unexpected combinations.</p>
+              </div>
+
+          </div>
+
+      </section>
+*/}
+      {/* review section ends */}
+
+
+      {/* footer section  */}
+
+{/*
+      <footer className="footer">
+
+          <section>
+
+              <div className="share">
+                  <a href="#" className="btn">facebook</a>
+                  <a href="#" className="btn">twitter</a>
+                  <a href="#" className="btn">instagram</a>
+                  <a href="#" className="btn">pinterest</a>
+                  <a href="#" className="btn">linkedin</a>
+              </div>
+
+          </section>
+
+      </footer>
+*/}
+      {/* scroll top button  */}
+      <a href="#home" className="fas fa-angle-up" id="scroll-top"></a>
+
+      {/* loader  */}
+      {/*
+      <div className="loader-container">
+          <img src="images/loader.gif" alt="">
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/}
+
+      {/* custom js file link  */}
+      {/* <script src="js/script.js"></script>  */}
+
     </>
   )
 }
---
+++ project-2/src/ImageComp.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+
+
+const ImageComp = ({ folder, index, imageClass }) => {
+    const [imagePath, setImagePath] = useState(null);
+    useEffect(() => {
+        const loadImage = async () => {
+          try {
+            const imageContextG = import.meta.globEager('./assets/images/g-imgs/*');
+            const imageContextP = import.meta.globEager('./assets/images/p-imgs/*');
+            const imageContextPic = import.meta.globEager('./assets/images/pic-imgs/*');
+            const imageContextSA = import.meta.globEager('./assets/images/s-imgs-a/*');
+            const imageContextSB = import.meta.globEager('./assets/images/s-imgs-b/*');
+            const imageContexts = {
+                'g-imgs': imageContextG,
+                'p-imgs': imageContextP,
+                'pic-imgs': imageContextPic,
+                's-imgs-a': imageContextSA,
+                's-imgs-b': imageContextSB
+            };
+
+            const imagePaths = Object.keys(imageContexts[folder]);
+            console.log(imagePaths)
+            
+            const selectedImagePath = imagePaths[index];
+            console.log(selectedImagePath)
+            // If the image path is found, set it in the state
+            if (selectedImagePath) {
+              const image = imageContexts[folder][selectedImagePath].default;
+              setImagePath(image);
+            } else {
+              setImagePath(null);
+            }
+          } catch (error) {
+            console.error('Error loading image:', error);
+            setImagePath(null);
+          }
+        };
+    
+        loadImage();
+      }, [folder, index]);
+
+      return (
+      <div>
+      {imagePath ? (
+        <img  className={imageClass} src={imagePath} alt={`Image at index ${index} in folder number ${folder}`} />
+      ) : (
+        <p>Image not found</p>
+      )}
+    </div>
+      )
+
+}
+
+export default ImageComp
